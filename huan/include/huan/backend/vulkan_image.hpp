ad84d49f9992145fa5ed390b592261f91280a3d2
qiyuewuyi2333 <duan20030920@gmail.com>
2025-05-02 22:28:12 +0800

    Refactor: ImageView goto Image and maybe we will have the dynamic view in the feature

--- huan/include/huan/backend/vulkan_image.hpp
+++ huan/include/huan/backend/vulkan_image.hpp
@@ -16,6 +16,9 @@ class ResourceSystem;
 
 namespace huan::vulkan
 {
+/**
+ * 如果你想设置当前Image的mipmap 为 1，只需要调用  setMipmapCount(1) 然后我们会自动重新生成 ImageView
+ */
 class Image
 {
     friend class ::huan::ResourceSystem;
@@ -26,12 +29,19 @@ public:
         Static,
         Dynamic
     };
+    // TODO: setBaseMipmapLevel 动态生成imageView 后续可能会增加缓存什么的 如果需要高速更改的话
 
 INNER_VISIBLE:
     vk::Image m_image;
+    vk::ImageView m_imageView;
     VmaAllocation m_allocation;
     WriteType m_writeType;
     vk::Extent3D m_extent;
+
+    /**
+     * 方便我们后续动态调整View
+     */
+    vk::ImageViewCreateInfo m_viewInfo;
 
 protected:
     Image();
